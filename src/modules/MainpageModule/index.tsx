59f7a7ee0c5619a902bf97b46854c989bf17acfa
Grace <gracekarina12@gmail.com>
2025-03-20 17:51:08 +0000

    feat: add main page module

---
+++ src/modules/MainpageModule/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { MainpageSection } from "./sections/MainpageSection";
+
+
+export function MainpageModule() {
+    return <MainpageSection />;
+};
+
---
+++ src/modules/MainpageModule/sections/MainpageSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function MainpageSection() {
+    return (
+        <div>Hello :D</div>
+    )
+}
